e301109c5913c76cd1f5b44c69d9ecda87de772a
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-08 11:48:56 +0400

    the Result is happen

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.test import TestCase
+from django.http import HttpResponse
 
 # Create your tests here.
+def HomePage(request):
+    return HttpResponse('home')
--- edit_update_form/urls.py
+++ edit_update_form/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.HomePage, name='home'),
+    path('', views.HomePage, name='home'),
+
+]
