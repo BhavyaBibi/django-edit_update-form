8a73a3f5954c053cc3f19e1e39e3cbf1dd7df5fb
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-09 11:37:48 +0400

    some waning received. remove the terminal section

--- edit_update_form/urls.py
+++ edit_update_form/urls.py
@@ -21,9 +21,9 @@ from app1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage, name='home'),
-    path('/insert_student', views.InsertStudent, name="insert"),
-    path('/update_all', views.update_all, name="update_all"),
-    path('/delete_data', views.delete_data, name="delete_data"),
+    path('insert_student', views.InsertStudent, name="insert"),
+    path('update_all', views.update_all, name="update_all"),
+    path('delete_data', views.delete_data, name="delete_data"),
 
 
 ]
